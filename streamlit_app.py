fbfd52fc3dac87d2f2e171ddef71cc716f41984d
jhonsito2371 <jhonbueno89@gmail.com>
2025-07-29 14:07:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,10 +174,11 @@ with col1:
     fig1 = px.pie(df_final, names="Estado Final", title="Distribución de Estado Final")
     st.plotly_chart(fig1, use_container_width=True)
 with col2:
-    conteo_revision = df_final["Revisión"].value_counts(dropna=True).reset_index()
+   conteo_revision = df_final["Revisión"].value_counts(dropna=True).reset_index()
 conteo_revision.columns = ["Estado Revisión", "Cantidad"]
 fig2 = px.bar(conteo_revision, x="Estado Revisión", y="Cantidad", title="Revisión por Estado")
-    st.plotly_chart(fig2, use_container_width=True)
+st.plotly_chart(fig2, use_container_width=True)
+
 
 # Descargar Excel
 buffer = BytesIO()
